0990767d82a7bde9e09d011897657ae9be45cc30
Eric Eaglstun <postpostmodern+pinecone@gmail.com>
2014-01-14 09:54:55 -0600

    filled in plugin header, htaccess update

--- _load.php
+++ _load.php
@@ -1,11 +1,11 @@
 <?
 /*
-Plugin Name: 
-Plugin URI: 
-Description: 
-Author: 
-Version: 0.0.1
-Author URI: 
+Plugin Name: 	plugin-empty
+Plugin URI: 	https://github.com/pinecone-dot-io/plugin-empty
+Description: 	a little bit of structure for your wordpress plugin
+Author: 		pinecone-dot-io
+Version:		0.0.1
+Author URI: 	http://pinecone.io
 */
 
 register_activation_hook( __FILE__, create_function("", '$ver = "5.3"; if( version_compare(phpversion(), $ver, "<") ) die( "This plugin requires PHP version $ver or greater be installed." );') );
